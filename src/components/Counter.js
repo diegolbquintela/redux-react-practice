137583433c6a93d715f194eeb1e03a7f64800f4f
Diego Quintela <diegolucasq@gmail.com>
2022-01-28 08:18:43 -0500

    added redux toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,6 +6,7 @@ const Counter = () => {
   const dispatch = useDispatch();
   //subscribing to the store using useSelector
   const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
 
   //dispatch handler functions
   const incrementHandler = () => {
@@ -15,12 +16,14 @@ const Counter = () => {
     dispatch({ type: "DECREMENT" });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "TOGGLE" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={decrementHandler}>Decrement</button>
