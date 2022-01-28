4dcd73cccc1a7e2abc0037835a6071da4b8211ef
Diego Quintela <diegolucasq@gmail.com>
2022-01-28 07:44:43 -0500

    action payload for dynamic incrementation

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,30 @@
+import { useSelector, useDispatch } from "react-redux";
+
 import classes from "./Counter.module.css";
 
-const Counter = (props) => {
+const Counter = () => {
+  const dispatch = useDispatch();
+  //subscribing to the store using useSelector
+  const counter = useSelector((state) => state.counter);
+
+  //dispatch handler functions
+  const incrementHandler = () => {
+    dispatch({ type: "INCREMENT", amount: 1 });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
